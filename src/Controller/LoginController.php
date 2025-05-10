f55572cac07b7f41236ae00e1563f9647ad36d00
yassinekolsi <yassine.kolsi@gmail.com>
2025-05-10 03:31:06 +0100

    bug fixes

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -1,28 +1,27 @@
 <?php
 
 namespace App\Controller;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 final class LoginController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
+        // ✅ Redirect if already logged in
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_home');
+        }
+
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
-
-        // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('login/index.html.twig', 
-        
-        
-        
-        [
+        return $this->render('login/index.html.twig', [
             'last_username' => $lastUsername,
             'error' => $error,
         ]);
@@ -31,7 +30,6 @@ final class LoginController extends AbstractController
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
     {
-        // controller can be blank: it will never be executed!
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+        throw new \LogicException('This method is intercepted by the firewall.');
     }
 }
